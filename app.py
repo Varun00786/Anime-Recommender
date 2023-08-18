cd3e7cbae5666d3b19cb39152f346efbc249b372
Varun00786 <111782331+Varun00786@users.noreply.github.com>
2023-08-18 23:36:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,19 @@ import joblib
 df=joblib.load('dfanime')
 tfidf_matrix=joblib.load('animev')
 from sklearn.metrics.pairwise import  cosine_similarity
+hide_menu="""
+<style>
+#MainMenu{visibility:hidden;
+}
+img {
+    width:500px;
+    height: 500px;
+}
+</style>
 
 
+"""
+st.set_page_config(layout="wide")
 def fetch_poster(movie_id):
     url = "https://api.jikan.moe/v4/anime/{}".format(movie_id)
     data = requests.get(url)
@@ -20,7 +31,7 @@ def fetch_poster(movie_id):
     except Exception as e:
         return str("Something went/n Wrong")
 
-    
+@st.cache_data
 def recommend(movie):
     index = df[df['English name'] == movie].index[0]
     sim_scores = list(enumerate(cosine_similarity(tfidf_matrix,tfidf_matrix[index])))
@@ -33,84 +44,62 @@ def recommend(movie):
         recommended_movie_names.append(df.iloc[i[0]]['English name'])
 
     return recommended_movie_names,recommended_movie_posters
+menu_choice = st.sidebar.radio("Select an option", ("Home", "About"))
+
+if menu_choice == "Home":
+    
+    st.markdown(hide_menu,unsafe_allow_html=True)
+    st.title("Welcome to the Homepage")
+    st.header('Anime Recommender')
+    movie_list = df['English name'].values
+    selected_movie = st.selectbox(
+        "Type or select a movie from the dropdown",
+        movie_list
+    )
 
 
-st.header('Anime Recommender')
-movie_list = df['English name'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
+    if st.button('Show Recommendation'):
+        recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+        
+        col1, col2, col3, col4, col5 = st.columns(5)
+        with col1:
+            st.text(recommended_movie_names[0])
+            if recommended_movie_posters[0]=="Something went/n Wrong":
+                st.error("!Oops Something went wrong")
+            else:
+                st.image(recommended_movie_posters[0],use_column_width='auto')
+            
+        with col2:
+            st.text(recommended_movie_names[1])
+            if recommended_movie_posters[1]=="Something went/n Wrong":
+                st.error("!Oops Something went wrong")
+            else:
+                st.image(recommended_movie_posters[1],use_column_width='auto')
+            
+        with col3:
+            st.text(recommended_movie_names[2])
+            if recommended_movie_posters[2]=="Something went/n Wrong":
+                st.error("!Oops Something went wrong")
+            else:
+                st.image(recommended_movie_posters[2],use_column_width='auto')
+            
+        with col4:
+            st.text(recommended_movie_names[3])
+            if recommended_movie_posters[3]=="Something went/n Wrong":
+                st.error("!Oops Something went wrong")
+            else:
+                st.image(recommended_movie_posters[3],use_column_width='auto')
+            
+        with col5:
+            st.text(recommended_movie_names[4])
+            if recommended_movie_posters[4]=="Something went/n Wrong":
+                st.error("!Oops Something went wrong")
+            else:
+                st.image(recommended_movie_posters[4],use_column_width='auto')
+            
+if menu_choice=="About":
+    st.title("About Us")
+    st.write("Learn more about our company and mission here.")
+    st.write("This is my first streamlit project")
+    st.write("It is an anime recommendation app that gives you five similar anime to the anime you choose.")
 
-
-if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    # tab1,tab2=st.tabs(["Tab 1", "Tab2"])
-    # with tab1:
-    #     tab1.write('one')
-    col1, col2, col3, col4, col5 = st.columns(5,gap="medium")
-    with col1:
-        st.text(recommended_movie_names[0])
-        if recommended_movie_posters[0]=="Something went/n Wrong":
-            st.text("!Oops Something")
-            st.text("went wrong")
-        else:
-            st.image(recommended_movie_posters[0],use_column_width='auto')
-        # st.text(recommended_movie_names[5])
-        # if recommended_movie_posters[5]=="Something went/n Wrong":
-        #     st.text("!Oops Something")
-        #     st.text("went wrong")
-        # else:
-        #     st.image(recommended_movie_posters[5])
-    with col2:
-        st.text(recommended_movie_names[1])
-        if recommended_movie_posters[1]=="Something went/n Wrong":
-            st.text("!Oops Something")
-            st.text("went wrong")
-        else:
-            st.image(recommended_movie_posters[1],use_column_width='auto')
-        # st.text(recommended_movie_names[6])
-        # if recommended_movie_posters[6]=="Something went/n Wrong":
-        #     st.text("!Oops Something")
-        #     st.text("went wrong")
-        # else:
-        #     st.image(recommended_movie_posters[6])
-    with col3:
-        st.text(recommended_movie_names[2])
-        if recommended_movie_posters[2]=="Something went/n Wrong":
-            st.text("!Oops Something")
-            st.text("went wrong")
-        else:
-            st.image(recommended_movie_posters[2],use_column_width='auto')
-        # st.text(recommended_movie_names[7])
-        # if recommended_movie_posters[7]=="Something went/n Wrong":
-        #     st.text("!Oops Something")
-        #     st.text("went wrong")
-        # else:
-        #     st.image(recommended_movie_posters[7])
-    with col4:
-        st.text(recommended_movie_names[3])
-        if recommended_movie_posters[3]=="Something went/n Wrong":
-            st.text("!Oops Something")
-            st.text("went wrong")
-        else:
-            st.image(recommended_movie_posters[3],use_column_width='auto')
-        # st.text(recommended_movie_names[8])
-        # if recommended_movie_posters[8]=="Something went/n Wrong":
-        #     st.text("!Oops Something")
-        #     st.text("went wrong")
-        # else:
-        #     st.image(recommended_movie_posters[8])
-    with col5:
-        st.text(recommended_movie_names[4])
-        if recommended_movie_posters[4]=="Something went/n Wrong":
-            st.text("!Oops Something")
-            st.text("went wrong")
-        else:
-            st.image(recommended_movie_posters[4],use_column_width='auto')
-        # st.text(recommended_movie_names[9])
-        # if recommended_movie_posters[9]=="Something went/n Wrong":
-        #     st.text("!Oops Something")
-        #     st.text("went wrong")
-        # else:
-        #     st.image(recommended_movie_posters[9])
